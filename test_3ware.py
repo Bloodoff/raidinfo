f03f8e352e5b3fec0d085983991ce561680e4dd4
Bloody <Bloody@its66.ru>
2017-01-20 08:35:55 +0500

    Test was fixed

--- test_3ware.py
+++ test_3ware.py
@@ -9,7 +9,7 @@ from lib.raid import RaidController
 from lib.raid_3ware import RaidController3ware
 
 
-FakeResponses = { '/u1sr/sbin/tw_cli show'           : 'testdata/3ware/1.txt',
+FakeResponses = { '/usr/sbin/tw_cli show'           : 'testdata/3ware/1.txt',
                   '/usr/sbin/tw_cli /c6 show'       : 'testdata/3ware/2.txt',
                   '/usr/sbin/tw_cli /c6/u0 show all': 'testdata/3ware/3.txt',
                   '/usr/sbin/tw_cli /c6/p0 show all': 'testdata/3ware/4.txt',
@@ -25,17 +25,17 @@ def fake_getOutput(cmd):
     lines = []
     testfile = FakeResponses.get(cmd, False)
     if not testfile:
-        print('Test file for command "{}" not found'.format(cmd))
+        raise Exception('Test file for command "{}" not found'.format(cmd))
     output = read_file('{}/{}'.format(os.path.dirname(__file__), testfile))
     for line in output:
         if not re.match(r'^$', line.strip()):
             lines.append(line.strip())
     return lines
 
+
 @mock.patch('os.path.isfile', return_value=True)
 @mock.patch('lib.helpers.getOutput', fake_getOutput)
-
-def test_3ware_1(mock_1):
+def test_3ware_1(mock):
     controllers = RaidController.probe()
     for controller in controllers:
         controller.printInfo()
